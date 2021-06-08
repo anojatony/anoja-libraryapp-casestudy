023914482e4c6354dbed1f8df67dcfc8b33d1053
anoja <anojatony@gmail.com>
2021-06-08 13:34:47 +0530

    First Commit : First Version

--- src/model/Userdata.js
+++ src/model/Userdata.js
@@ -3,11 +3,12 @@
 const mongoose = require('mongoose');
 
 // Database connection (connecting server with database using mongoose package)
-mongoose.connect('mongodb://localhost:27017/libraryapp', { useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.set('useCreateIndex', true);
-// mongoose.connect('mongodb+srv://userone:userone@ictak-files.utn4t.mongodb.net/LIBRARY?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/libraryapp', { useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.set('useCreateIndex', true);
 
-// var passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb+srv://userone:userone@ictak-files.utn4t.mongodb.net/libraryapp?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // Schema definition
 const Schema=mongoose.Schema;
